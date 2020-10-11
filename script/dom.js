a9158f1f59c29022a5712041f0d5ce0dcefb07d3
Alwan Yusuf <aalyusuf12@gmail.com>
2020-10-12 01:22:13 +0700

    25%

--- script/dom.js
+++ script/dom.js
@@ -1,7 +1,26 @@
 const getComputerChoice = () => {
     const computer = Math.random()
 
-    if (computer < 0.34) return 'batu'
-    if (computer >= 0.34 && computer < 0.67) return 'gunting'
-    return 'scissor'
+    if (computer < 0.34) return 'rock'
+    if (computer >= 0.34 && computer < 0.67) return 'scissor'
+    return 'paper'
 }
+
+const getResult = (player, computer) => {
+    if (player == computer) return 'DRAW'
+    if (player == 'rock') return (computer == 'scissor') ? 'WINNER' : "LOSER"
+    if (player == 'scissor') return (computer == 'rock') ? 'LOSER' : 'WINNER'
+    if (player == 'paper') return (computer == 'scissor') ? 'LOSER' : 'WINNER'
+}
+
+const playerChoice = document.querySelectorAll('.player')
+playerChoice.forEach((choice) => {
+    choice.addEventListener('click', () => {
+        const playerChoice = choice.classList[0]
+        const computerChoice = getComputerChoice()
+        console.log(playerChoice)
+        console.log(computerChoice)
+
+
+    })
+})
